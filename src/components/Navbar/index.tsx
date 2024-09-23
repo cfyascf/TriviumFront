7116a534d99ae6c6d8b777b1c67721d9018dd2c1
yasmi <yas1cf1@gmail.com>
2024-09-23 11:53:46 -0300

    home page under development

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,30 @@
+import { useState } from "react";
 import styled from "./styles.module.sass";
 
 const Navbar = () => {
+    const [optionsShowing, setOptionsShowing] = useState(false);
+    const [configShowing, setConfigShowing] = useState(false);
+
     return (
         <div className={styled.navbar_container}>
             <div className={styled.navbar}>
                 <button className={styled.button}>
-                    <span className="material-symbols-rounded">home</span>
+                    <span className="material-symbols-outlined">home</span>
                 </button>
                 <button className={styled.button}>
-                    <span className="material-symbols-rounded">add_circle</span>
+                    <span className="material-symbols-outlined" onClick={() => setOptionsShowing(!optionsShowing)}>add_circle</span>
                 </button>
+                <div className={optionsShowing ? styled.options : styled._hidden}>
+                    <p className={styled.option}>Create room</p>
+                    <p className={styled.option}>Create form</p>
+                </div>
             </div>
             <div className={styled.navbar}>
                 <button className={styled.button}>
-                    <span className="material-symbols-rounded">settings</span>
+                    <span className="material-symbols-outlined">settings</span>
+                    {/* <div className={styled.config}>
+                        <p className={styled.option}>Logout</p>
+                    </div> */}
                 </button>
             </div>
         </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,30 @@
 import styled from "./styles.module.sass";
 import Navbar from "../../components/Navbar";
+import pp from "/profile_picture.png";
 
 const Home = () => {
     return (
         <>
             <Navbar/>
             <div className={styled.page}>
+                <div className={styled.profile}>
+                    <div className={styled.user}>
 
+                        <img src={pp} className={styled.profile_picture}></img>
+                        <div className={styled.data}>
+                            <h1 className={styled.name}>Name Lastname</h1>
+                            <p>email@gmail.com</p>
+                        </div>
+                    </div>
+                    <div className={styled.nav}>
+                        <div className={styled.button}><span className="material-symbols-outlined">Add</span></div>
+                        <div className={styled.button}><span className="material-symbols-outlined">Draw</span></div>
+                        <div className={styled.button}><span className="material-symbols-outlined">Edit</span></div>
+                    </div>
+                </div>
+                <div className={styled.history}>
+
+                </div>
             </div>
         </>
     );
