d4912992750dfde57c38939a311e8bdc6843c70f
silvamariaeduarda02@gmail.com <yasmin.agostinho.012@gmail.com>
2024-09-23 10:36:21 -0300

    desde

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -11,7 +11,22 @@ export const Login = () => {
             </div>
             <div className={styled.form_section}>
                 <div className={styled.form_container}>
-
+                    <div>
+                        <h1 className={styled.form_title}>Sign Up!</h1>
+                    </div>
+                    <form action="">
+                        <div className={styled.form_input}>
+                            <label htmlFor="">E-mail</label>
+                            <input className={styled.input} type="email" />
+                        </div>
+                        <div className={styled.form_input}>
+                            <label htmlFor="">Password</label>
+                            <input className={styled.input} type="password" name="" id="" />
+                        </div>
+                        <div className={styled.form_button}>
+                            <button className={styled.button}>Start</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
