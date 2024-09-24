db279358d5149e43a2334c941d3c267e0e5dd188
yasmi <yas1cf1@gmail.com>
2024-09-24 14:23:04 -0300

    brand name uploaded

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,12 @@
 import styled from "./styles.module.sass";
+import brand from "/brand_name_yellow.png";
 
 const Login = () => {
     return (
         <div className={styled.page}>
             <div className={styled.title_section}>
                 <div className={styled.title_container}>
-                    <p className={styled.title}>Welcome to </p><p className={styled.title}><span className={styled.highlight}>Trivium</span>!</p>
+                    <p className={styled.title}>Welcome to </p><img className={styled.brand} src={brand}></img>
                     <p className={styled.description}>Trivium is a platform to test yourself against your friends in a insane knowledge battle!</p>
                 </div>
                 <div className={styled.img_section}>
