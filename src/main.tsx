fc0c9d7db14d4b42a5b26a0dfcb44da0700380e0
yasmi <yas1cf1@gmail.com>
2024-10-29 10:19:31 -0300

    speed insghts

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import mainRouter from './routes/main.routes'
+import {SpeedInsights} from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <SpeedInsights/>
     <RouterProvider router={mainRouter}/>
   </StrictMode>,
 )
