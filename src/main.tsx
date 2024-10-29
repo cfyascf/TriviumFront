06dee9043ad4fcefea75fc9989ff2d15fc768062
yasmi <yas1cf1@gmail.com>
2024-10-29 10:51:28 -0300

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import mainRouter from './routes/main.routes'
-import {SpeedInsights} from '@vercel/speed-insights/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <SpeedInsights/>
+    <Analytics/>
     <RouterProvider router={mainRouter}/>
   </StrictMode>,
 )
