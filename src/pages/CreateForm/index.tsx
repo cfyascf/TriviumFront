93a6208cc5b458fa651d26f2ff8c0e61d9cc86be
yasmi <yas1cf1@gmail.com>
2024-11-08 11:07:35 -0300

    creating quesion on form screen

--- src/pages/CreateForm/index.tsx
+++ src/pages/CreateForm/index.tsx
@@ -1,10 +1,25 @@
-import { Checkbox } from "../../components/CreateForm/Checkbox"
+import { useState } from "react"
+import { Option } from "../../components/CreateForm/Option"
 import { TextInput } from "../../components/CreateForm/TextInput"
 import Navbar from "../../components/Navbar"
 import { Page } from "../../components/Page"
+import { SelectBox } from "../../components/CreateForm/SelectBox"
 import styled from "./styles.module.sass"
 
+export interface IOption {
+    right: boolean,
+    desc: string,
+    index: number
+}
+
 export const CreateForm = () => {
+    const [selectedValue, setSelectedValue] = useState<number>(4)
+    const [options, setOptions] = useState<IOption[]>([])
+
+    const handleSelect = (option: IOption) => {
+        setOptions([...options, option])
+    }
+
     return (
         <>
             <Navbar/>
@@ -12,19 +27,25 @@ export const CreateForm = () => {
                 <div className={styled.content}>
                     <h1 className={styled.title}>Create form</h1>
                     <div className={styled.data}>
-                        <h1>Form title</h1>
-                        <p>Form description</p>
+                        <input className={styled.title_input} type="text" placeholder="Form title here.."/>
+                        <input className={styled.subtitle_input} type="text" placeholder="Form description here..."/>
                     </div>
-                    <div>
+                    <div className={styled.question_container}>
                         <div className={styled.question}>
                             <div className={styled.header}>
-                                <TextInput label="Insira aqui sua pergunta"/>
-                                <div>Two Options</div>
+                                <TextInput label="Question description here..."/>
+                                <SelectBox selectedValue={selectedValue} setSelectedValue={setSelectedValue}/>
                             </div>
                             <div className={styled.options}>
-                                <Checkbox label="Option 1"/>
-                                <Checkbox label="Option 2"/>
+                                {
+                                    Array.from({ length: selectedValue }, (_, index) => (
+                                        <Option handleSelect={{handleSelect, index}}/>
+                                    ))
+                                }
                             </div>
+                        </div>
+                        <div>
+
                         </div>
                     </div>
                 </div>
