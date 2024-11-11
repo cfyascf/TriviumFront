afb4ad5e309287803ea498bf729f8748de3a4530
yasmi <yas1cf1@gmail.com>
2024-11-11 10:36:08 -0300

    screen ready

--- src/pages/CreateForm/index.tsx
+++ src/pages/CreateForm/index.tsx
@@ -5,6 +5,7 @@ import Navbar from "../../components/Navbar"
 import { Page } from "../../components/Page"
 import { SelectBox } from "../../components/CreateForm/SelectBox"
 import styled from "./styles.module.sass"
+import { Question } from "../../components/CreateForm/Question"
 
 export interface IOption {
     right: boolean,
@@ -13,41 +14,24 @@ export interface IOption {
 }
 
 export const CreateForm = () => {
-    const [selectedValue, setSelectedValue] = useState<number>(4)
-    const [options, setOptions] = useState<IOption[]>([])
-
-    const handleSelect = (option: IOption) => {
-        setOptions([...options, option])
-    }
+    const [questions, setQuestions] = useState(1) 
+    console.log(questions)
 
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <Page>
                 <div className={styled.content}>
                     <h1 className={styled.title}>Create form</h1>
                     <div className={styled.data}>
-                        <input className={styled.title_input} type="text" placeholder="Form title here.."/>
-                        <input className={styled.subtitle_input} type="text" placeholder="Form description here..."/>
+                        <input className={styled.title_input} type="text" placeholder="Form title here.." />
+                        <input className={styled.subtitle_input} type="text" placeholder="Form description here..." />
                     </div>
-                    <div className={styled.question_container}>
-                        <div className={styled.question}>
-                            <div className={styled.header}>
-                                <TextInput label="Question description here..."/>
-                                <SelectBox selectedValue={selectedValue} setSelectedValue={setSelectedValue}/>
-                            </div>
-                            <div className={styled.options}>
-                                {
-                                    Array.from({ length: selectedValue }, (_, index) => (
-                                        <Option handleSelect={{handleSelect, index}}/>
-                                    ))
-                                }
-                            </div>
-                        </div>
-                        <div>
-
-                        </div>
-                    </div>
+                    {
+                        Array.from({ length: questions }, (_, index) => (
+                            <Question questions={questions} setQuestions={setQuestions} />
+                        ))
+                    }
                 </div>
             </Page>
         </>
