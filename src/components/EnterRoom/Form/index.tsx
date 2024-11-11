6caffd09f542b4a261a081c82dc75ac11016b8f9
Maria <silvamariaeduarda92@gmail.com>
2024-11-11 11:08:24 -0300

    started enter a room

--- src/components/EnterRoom/Form/index.tsx
+++ src/components/EnterRoom/Form/index.tsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
 import styled from "./styles.module.sass";
 
 export const Form = () => {
+    const [pin, setPin] = useState("");
+
+    // const handleSend = async () => {
+    //     const userId = sessionStorage.getItem("@USERID");
+
+    //     const data = { userId, matchId }
+
+    //     try {
+    //         const response = await API.post(`/match/adduser`,{
+    //             headers: {
+    //             'Authorization': `Bearer ${sessionStorage.getItem("@TOKEN")}`,
+    //             }});
+
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
+
+
     return (
         <div className={styled.form_container}>
-            <input placeholder="Room name"/>
+            <input 
+                placeholder="Room name" 
+                value={pin} 
+                onChange={(e) => setPin(e.target.value)} 
+                required 
+            />
             <button>Create</button>
         </div>
     );
